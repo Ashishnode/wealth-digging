2729558c670230abfc029a0d563c8b36509581e4
ashishnode <ashishweb1911@gmail.com>
2024-10-19 21:43:39 +0530

    featmenu

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footor'>
+     <h1>Coin</h1>
+     <h1>About</h1>
+     <h1>Blog</h1>
+     <h1>wallet</h1>
+     <h1>Prime</h1>
+    </div>
+  )
+}
+
+export default Footer
